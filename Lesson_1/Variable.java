d262d0ac630a4445b68244839a2603d6dd22503e
Mihail Volkomorov <mih21091984@gmail.com>
2019-11-20 21:36:45 +0300

    Первое ДЗ

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,30 @@
+public class Variable {
+	public static void main (String[] args){
+		byte a;
+		short b;
+		int c;
+		long d;
+		float e;
+		double f;
+		char g;
+		boolean h;
+
+		a = -10; /* при какой отрицательной температуре может работать */
+		b = 121; /* объём ЖМД */
+		c = -2018; /* год производства */
+		d = 1000000000; /* количество поддерживаемых цветов */
+		e = 3.141592f; /* число Пи */
+		f = 10.15d; /* версия операционной системы */
+		g = 'A'; /* первая буква фирмы производителя */
+		h = false; /* ложь */
+	
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}
