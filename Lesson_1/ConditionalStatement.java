91bf7d97ebf15e443076d9454278af23d2d1d065
Mihail Volkomorov <mih21091984@gmail.com>
2019-11-22 20:32:52 +0300

    Правка второго ДЗ

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,32 +1,31 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
 		int personAge = 25;
-		boolean isPersonSexMen = true;
-		float personHeight = 1.78f;
-		char personFirstLetterName = 'М';
-
 		if (personAge > 20) {
 			System.out.println ("Человек старше 20 лет");
 		}
-		if (isPersonSexMen) {
+
+		boolean isMan = true;
+		if (isMan) {
 			System.out.println ("Человек - мужчина");
 		}
-		if (isPersonSexMen != true) {
+		if (!isMan) {
 			System.out.println ("Человек - не мужчина");
 		}
+
+		float personHeight = 1.78f;
 		if (personHeight < 1.80) {
 			System.out.println ("Рост человека меньше 180 сантиметров");
-		} 
-		else {
+		} else {
 			System.out.println ("Рост человека больше 180 сантиметров");
 		}
+
+		char personFirstLetterName = 'М';
 		if (personFirstLetterName == 'A') {
 			System.out.println ("Первая буква имени человека не 'А'");
-		} 
-		else if (personFirstLetterName == 'I') {
+		} else if (personFirstLetterName == 'I') {
 			System.out.println ("Первая буква имени человека не 'I'");
-		}
-		else {
+		} else {
 			System.out.println ("Скорее всего первая буква имени человека 'M'");
 		}
 	}
