8704e116202478fd5dec8bae5d4001b98a1d90a6
Mihail Volkomorov <mih21091984@gmail.com>
2019-12-10 22:49:21 +0300

    Правка калькулятора

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,28 +2,28 @@ public class Calculator {
 	public static void main(String[] args) {
 		int a = 9;
 		int b = 5; 
-		char c = '+';
+		char mathOperation = '+';
 
-		if (c == '+') {
+		if (mathOperation == '+') {
 			System.out.println(a + b);
-		} else if (c == '-') {
+		} else if (mathOperation == '-') {
 			System.out.println(a - b);			
-		} else if (c == '*') {
+		} else if (mathOperation == '*') {
 			System.out.println(a * b);			
-		} else if (c == '/') {
+		} else if (mathOperation == '/') {
 			System.out.println(a / b);			
-		} else if (c == '^') {
-			System.out.println(degree(a,b));			
-		} else if (c == '%') {
+		} else if (mathOperation == '^') {
+			System.out.println(toDoDegree(a, b));			
+		} else if (mathOperation == '%') {
 			System.out.println(a % b);			
 		}
 	}
 
-	private static int degree(int z, int y) {
+	private static int toDoDegree(int z, int y) {
         	int result = 1;
         	for (int i = 0; i < y; i++) {
         	    result *= z;
         	}
-        return result;
+        	return result;
    	}
 }
