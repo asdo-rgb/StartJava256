c9fc3188fc7af214a002e2c90bb1d2ebb13659fe
Mihail Volkomorov <mih21091984@gmail.com>
2019-12-15 22:58:40 +0300

    Вторая доработка игры Угадай число

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -1,31 +1,25 @@
 public class GuessNumber {
 	public static void main(String[] args) {
-		int numberPlayer = 0;
-		int numberComputer = 0;
+		int playerNumber = 0;
+		int computerNumber = 0;
 
-		numberComputer = (int) (Math.random() * 100);
+		computerNumber = (int) (Math.random() * 100);
 		System.out.println("Компьютер загадывает число от 0 до 99...");
 
-		while (numberPlayer != numberComputer) {
-			System.out.println("Число, которое нужно угадать - " + numberComputer);
+		while (playerNumber != computerNumber) {
+			System.out.println("Число, которое нужно угадать - " + computerNumber);
 
-			numberPlayer = (int) (Math.random() * 100);
-			System.out.println("Игрок думает, что это " + numberPlayer);
-		
-			if (numberPlayer == numberComputer) {
-				System.out.println("Игрок угадал!");
-				break; 
-			} else {
-				if (numberPlayer > numberComputer) {
-					System.out.println("Число игрока больше, чем загаданное компьютером");
-					System.out.println("Игрок должен попробовать еще раз.");
-				}
-				if (numberPlayer < numberComputer) {
+			playerNumber = (int) (Math.random() * 100);
+			System.out.println("Игрок думает, что это " + playerNumber);
+
+			if (playerNumber > computerNumber) {
+				System.out.println("Число игрока больше, чем загаданное компьютером");
+				System.out.println("Игрок должен попробовать еще раз.");
+				} else if (playerNumber < computerNumber) {
 					System.out.println("Число игрока меньше, чем загаданное компьютером");
 					System.out.println("Игрок должен попробовать еще раз.");
-				}
-				
-			}	
-		}
+					}
+			}
+	System.out.println("Игрок угадал!");
 	}	
 }
