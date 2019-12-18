d18b488530c7e466e9afbe02f5a5d31387b87479
Mihail Volkomorov <mih21091984@gmail.com>
2019-12-18 22:33:05 +0300

    Вывод символов Unicode

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,11 @@
+public class Unicode {
+	public static void main(String[] args) {
+		char symbol;
+		int i = 9398;
+		while (i<10179) {
+			symbol = (char) i;
+			System.out.println(symbol);
+			i++;
+		}
+	}
+}
