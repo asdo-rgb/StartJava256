7138a65caa3a1df924eda7e6236000032e1c706e
Mihail Volkomorov <mih21091984@gmail.com>
2019-12-18 23:02:54 +0300

    Правка задачи Вывод символов Unicode

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,11 +1,7 @@
 public class Unicode {
 	public static void main(String[] args) {
-		char symbol;
-		int i = 9398;
-		while (i<10179) {
-			symbol = (char) i;
-			System.out.println(symbol);
-			i++;
+		for (int i = 9398; i <= 10178; i++) {
+			System.out.println((char) i);
 		}
 	}
 }
