23db8b1938fc48bcf0a4115cd2d02c0c63fbabf5
Mihail Volkomorov <mih21091984@gmail.com>
2019-12-28 18:41:18 +0300

    Создание класса Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+	char sex = 'м';
+    String name = "Иван";
+    float height = 1.85f;
+    float weight = 78.5f;
+    int age = 30;
+
+    void move() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    boolean speak() {
+
+    }
+
+    void learnJava() {
+
+    }
+}
